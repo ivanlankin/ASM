f5f14934fb6a6b5e0a3dbefed26a8a9e117f5b0c
ivanlankin <34132072+ivanlankin@users.noreply.github.com>
2020-10-08 16:12:42 +0400

    to chto nuzhno

--- asm/task2.asm
+++ asm/task2.asm
@@ -2,16 +2,46 @@
                              ;размером не более 64Кб
 .stack 100h                  ;Сегмент стека размером 100h (256 байт)
 .data                        ;Начало сегмента данных
-Hello db 'Lankin Ivan 251$'
+UserName db 'Lankin Ivan 251', 0Dh, 0Ah, '$'
 .code                        ;Начало сегмента кода
 start:                       ;Точка входа в программу start 
                              ;Предопределенная метка @data обозначает
                              ;адрес сегмента данных в момент запуска программы,
 mov AX, @data                ;который сначала загрузим в AX,
 mov DS,AX                    ;а затем перенесем из AX в DS
-mov AH,09h
-mov DX,offset Hello
-int 21h
+mov DX,offset UserName
+call Out_string
+call print_two_digits
+
 mov AX,4C00h
 int 21h
+
+print_two_digits proc
+		mov BL, 04h
+		call print_char
+		mov BL, 20h
+		mov AH, 02h
+		mov DL, BL
+		int 21h
+		mov BL, 08h
+		call print_char
+		ret
+print_two_digits endp
+
+print_char proc
+		add BL, 30h
+		mov AH, 02h
+		mov DL, BL
+		int 21h
+		ret
+print_char endp
+
+Out_string proc
+		mov ah,09h
+		int 21h
+		ret
+Out_string endp
+
+
+
 end start
